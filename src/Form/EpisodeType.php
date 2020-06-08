7c1afdba56d3ada4ba4cc7bd2211889acbd8a7fc
Carolinefourcade <caroannab33@gmail.com>
2020-06-08 10:39:18 +0200

    Symfony Quest 12

---
+++ src/Form/EpisodeType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Episode;
+use App\Entity\Season;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EpisodeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('number', null, ['label' => 'Episode N°'])
+            ->add('synopsis')
+            ->add('season', EntityType::class, [
+                'choice_label' => function (Season $season) {
+                    return $season->getProgram()->getTitle() . ' ' . $season->getNumber();
+                },
+                'class' => Season::class,
+            ]);
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Episode::class,
+        ]);
+    }
+}
